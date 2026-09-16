50daccdecc5265bf23b97ad08ba6e529bfa91f14
lolologic <m.perezperez87@gmail.com>
2026-09-16 16:29:01 +0200

    OOP-Grundlagen mit Teilnehmerklasse umgesetzt

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,78 @@
-import 'package:oop1/teilnehmer.dart' as oop1;
+import 'package:oop1/teilnehmer.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${oop1.calculate()}!');
+void main() {
+  final teilnehmer = erstelleTeilnehmerListe();
+  ausgabeTeilnehmerListe(teilnehmer);
+}
+
+List<Teilnehmer> erstelleTeilnehmerListe() {
+  final teilnehmer = <Teilnehmer>[
+    Teilnehmer(
+      vorname: 'Max',
+      nachname: 'Mustermann',
+      geburtsdatum: DateTime(2000, 1, 1),
+    ),
+    Teilnehmer(
+      vorname: 'Anna',
+      nachname: 'Schmidt',
+      geschlecht: 'weiblich',
+      geburtsdatum: DateTime(1998, 3, 14),
+      abschlussnote: 2,
+    ),
+    Teilnehmer(
+      vorname: 'Mehmet',
+      nachname: 'Yilmaz',
+      geschlecht: 'männlich',
+      geburtsdatum: DateTime(2001, 7, 22),
+    ),
+    Teilnehmer(
+      vorname: 'Alex',
+      nachname: 'Meyer',
+      geburtsdatum: DateTime(1995, 11, 5),
+      abschlussnote: 1,
+    ),
+    Teilnehmer(
+      vorname: 'Sophie',
+      nachname: 'Wagner',
+      geschlecht: 'weiblich',
+      geburtsdatum: DateTime(2003, 1, 30),
+    ),
+  ];
+
+  return teilnehmer;
+}
+
+void ausgabeTeilnehmerListe(List<Teilnehmer> teilnehmer) {
+  for (int i = 0; i < teilnehmer.length; i++) {
+    print('Vorname: ${teilnehmer[i].vorname}');
+    print('Nachname: ${teilnehmer[i].nachname}');
+
+    if (teilnehmer[i].geschlecht != null) {
+      print('Geschlecht: ${teilnehmer[i].geschlecht}');
+    }
+
+    print('Alter: ${berechneAlter(teilnehmer[i])}');
+
+    if (teilnehmer[i].abschlussnote != null) {
+      print('Abschlussnote: ${teilnehmer[i].abschlussnote}');
+    }
+
+    print('');
+  }
+}
+
+int berechneAlter(Teilnehmer teilnehmer) {
+  final dateNow = DateTime.now();
+
+  int alter = dateNow.year - teilnehmer.geburtsdatum.year;
+
+  if (
+    (dateNow.month < teilnehmer.geburtsdatum.month) || 
+    (dateNow.month == teilnehmer.geburtsdatum.month && 
+    dateNow.day < teilnehmer.geburtsdatum.day)
+    ) {
+    alter -= 1;
+  }
+
+  return alter;
 }
--- lib/teilnehmer.dart
+++ lib/teilnehmer.dart
@@ -1,3 +1,15 @@
-int calculate() {
-  return 6 * 7;
+class Teilnehmer {
+  String vorname;
+  String nachname;
+  String? geschlecht;
+  DateTime geburtsdatum;
+  int? abschlussnote;
+
+  Teilnehmer({
+    required this.vorname,
+    required this.nachname,
+    this.geschlecht,
+    required this.geburtsdatum,
+    this.abschlussnote
+    });
 }
